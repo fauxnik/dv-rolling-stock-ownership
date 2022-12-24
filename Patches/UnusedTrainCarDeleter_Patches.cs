5d0ed071fd6d9ef6542e43c01e69280c44e21f7c
Niko Fox <niko@fauxnik.me>
2022-12-24 00:18:16 -0800

    fix NRE when despawning train cars
    train cars were getting doubly marked for deletion by the station controller when it cleared jobs in response to the player moving out of range

--- Patches/UnusedTrainCarDeleter_Patches.cs
+++ Patches/UnusedTrainCarDeleter_Patches.cs
@@ -1,4 +1,5 @@
 ﻿using Harmony12;
+using System.Collections.Generic;
 
 namespace DVOwnership.Patches
 {
@@ -20,6 +21,9 @@ namespace DVOwnership.Patches
             var UnusedTrainCarDeleter_AreDeleteConditionsFulfilled = AccessTools.Method(typeof(UnusedTrainCarDeleter), "AreDeleteConditionsFulfilled");
             var UnusedTrainCarDeleter_AreDeleteConditionsFulfilled_Prefix = AccessTools.Method(typeof(UnusedTrainCarDeleter_Patches), "AreDeleteConditionsFulfilled_Prefix");
             DVOwnership.Patch(UnusedTrainCarDeleter_AreDeleteConditionsFulfilled, prefix: new HarmonyMethod(UnusedTrainCarDeleter_AreDeleteConditionsFulfilled_Prefix));
+            var UnusedTrainCarDeleter_MarkForDelete = AccessTools.Method(typeof(UnusedTrainCarDeleter), "MarkForDelete", new System.Type[] { typeof(TrainCar) });
+            var UnusedTrainCarDeleter_MarkForDelete_Prefix = AccessTools.Method(typeof(UnusedTrainCarDeleter_Patches), nameof(MarkForDelete_Prefix));
+            DVOwnership.Patch(UnusedTrainCarDeleter_MarkForDelete, prefix: new HarmonyMethod(UnusedTrainCarDeleter_MarkForDelete_Prefix));
         }
 
         static bool AreDeleteConditionsFulfilled_Prefix(ref bool __result, TrainCar trainCar)
@@ -36,6 +40,16 @@ namespace DVOwnership.Patches
 
             __result = equipment.IsMarkedForDespawning;
             return false;
+        }
+
+        static bool MarkForDelete_Prefix(TrainCar unusedTrainCar, List<TrainCar> ___unusedTrainCarsMarkedForDelete)
+        {
+            if (___unusedTrainCarsMarkedForDelete.Contains(unusedTrainCar))
+            {
+                DVOwnership.LogWarning($"Attempting to mark {unusedTrainCar.ID} for deletion, but it's already marked for deletion.");
+                return false;
+            }
+            return true;
         }
     }
 }
