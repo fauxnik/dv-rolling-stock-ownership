2d8004949143d2302a4f4a652aace03cfa7debc6
Niko Fox <niko@fauxnik.me>
2023-09-21 23:40:03 -0700

    Revise popup message
    Previously, the popup message was showing with a locked cursor.
    This caused it to be impossible to close any way other than pressing the Esc key.
    Adding a delay gives the game time to initialize before displaying the popup.
    This appears to fix the locked cursor issue.

--- MessageBox.cs
+++ MessageBox.cs
@@ -8,14 +8,24 @@ namespace DVOwnership
 {
 	public static class MessageBox
 	{
-		public static void ShowPopupOk(string message, string title = "", PopupClosedDelegate? onClose = null)
+		public static void ShowPopupOk(string message, string title = "", string positive = "Ok", PopupClosedDelegate? onClose = null)
 		{
-			ShowPopup(uiReferences.popupOk, new PopupLocalizationKeys { titleKey = title, labelKey = message }, onClose);
+			// TODO: Should ShowPopupOk even accept a title argument? The prefab doesn't appear to display it.
+			ShowPopup(uiReferences.popupOk, new PopupLocalizationKeys {
+				titleKey = title,
+				labelKey = message,
+				positiveKey = positive
+			}, onClose);
 		}
 
-		public static void ShowPopupYesNo(string message, string title = "", PopupClosedDelegate? onClose = null)
+		public static void ShowPopupYesNo(string message, string title = "", string positive = "Yes", string negative = "No", PopupClosedDelegate? onClose = null)
 		{
-			ShowPopup(uiReferences.popupYesNo, new PopupLocalizationKeys { titleKey = title, labelKey = message }, onClose);
+			ShowPopup(uiReferences.popupYesNo, new PopupLocalizationKeys {
+				titleKey = title,
+				labelKey = message,
+				positiveKey = positive,
+				negativeKey = negative,
+			}, onClose);
 		}
 
 		public static void ShowPopup3Buttons(string message, string title = "", string positive = "Yes", string negative = "No", string abort = "Abort", PopupClosedDelegate? onClose = null)
--- StartingConditions.cs
+++ StartingConditions.cs
@@ -1,9 +1,12 @@
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using DV.InventorySystem;
+using DV.Simulation.Cars;
 using DV.ThingTypes;
 using DV.ThingTypes.TransitionHelpers;
 using DV.Utils;
+using UnityEngine;
 
 namespace DVOwnership;
 
@@ -18,47 +21,81 @@ internal static class StartingConditions
 
 	internal static void Verify()
 	{
+		bool isShuntingLicenseChanged = false;
 		LicenseManager licenseManager = SingletonBehaviour<LicenseManager>.Instance;
 		if (!licenseManager.IsJobLicenseAcquired(TransitionHelpers.ToV2(JobLicenses.Shunting)))
 		{
 			licenseManager.AcquireJobLicense(TransitionHelpers.ToV2(JobLicenses.Shunting));
+			isShuntingLicenseChanged = true;
 		}
 
 		RollingStockManager rollingStockManager = SingletonBehaviour<RollingStockManager>.Instance;
 		if (rollingStockManager.AllEquipment.Count == 0)
 		{
-			List<RailTrack> allTracks = new List<RailTrack>(SingletonBehaviour<RailTrackRegistry>.Instance.AllTracks);
-			RailTrack playerHomeTrack = allTracks.Find(track => track.logicTrack.ID.FullID == playerHomeTrackID);
-
-			Inventory inventory = SingletonBehaviour<Inventory>.Instance;
-			UnusedTrainCarDeleter unusedTrainCarDeleter = SingletonBehaviour<UnusedTrainCarDeleter>.Instance;
-			CarSpawner carSpawner = SingletonBehaviour<CarSpawner>.Instance;
-			TrainCarLivery starterLoco = TrainCarType.LocoShunter.ToV2();
-			carSpawner.SpawnCarTypesOnTrackRandomOrientation(new List<TrainCarLivery> { starterLoco }, playerHomeTrack, true, true)
-				.ForEach(car => {
-					rollingStockManager.Add(Equipment.FromTrainCar(car));
-					if (!car.trainset.cars.Any(car => car.brakeSystem.brakeset.anyHandbrakeApplied))
-					{
-						car.brakeSystem.handbrakePosition = 1f;
-					}
-				});
-
-			foreach(TrainCarLivery livery in startingWagons)
-			{
-				inventory.AddMoney(CommsRadioEquipmentPurchaser.CalculateCarPrice(livery.v1));
-			}
-
-			MessageBox.ShowPopupOk(
-				title: "Rolling Stock Ownership",
-				message: string.Join(" ", new string[] {
-					"It looks like this is the first time RSO has been used with this save.",
-					"If you didn't already own it, the shunting license has been given to you.",
-					"You will also find a starter locomotive outside the player home and some extra cash in your wallet.",
-					"Use the extra cash to buy a few wagons as they won't magically appear at stations anymore.",
-					"A purchase mode has been added to your Comms Radio for this purpose.",
-					"\n\nGood luck and have fun!"
-				})
-			);
+			CoroutineManager.Instance.StartCoroutine(AcquireStarterEquipment(rollingStockManager));
+			CoroutineManager.Instance.StartCoroutine(ShowIntroductoryPopup(isShuntingLicenseChanged));
 		}
+	}
+
+	private static IEnumerator AcquireStarterEquipment(RollingStockManager rollingStockManager)
+	{
+		List<RailTrack> allTracks = new List<RailTrack>(SingletonBehaviour<RailTrackRegistry>.Instance.AllTracks);
+		RailTrack playerHomeTrack = allTracks.Find(track => track.logicTrack.ID.FullID == playerHomeTrackID);
+
+		Inventory inventory = SingletonBehaviour<Inventory>.Instance;
+		UnusedTrainCarDeleter unusedTrainCarDeleter = SingletonBehaviour<UnusedTrainCarDeleter>.Instance;
+		CarSpawner carSpawner = SingletonBehaviour<CarSpawner>.Instance;
+		TrainCarLivery starterLoco = TrainCarType.LocoShunter.ToV2();
+		IEnumerable<Equipment> roster = carSpawner.SpawnCarTypesOnTrackRandomOrientation(new List<TrainCarLivery> { starterLoco }, playerHomeTrack, true, true)
+			.Select(Equipment.FromTrainCar);
+
+		foreach (Equipment equipment in roster)
+		{
+			yield return null;
+
+			SimController? simController = equipment.GetTrainCar()?.GetComponent<SimController>();
+			BaseControlsOverrider? baseControlsOverrider = simController?.controlsOverrider;
+
+			if (baseControlsOverrider != null)
+			{
+				BaseControlsOverrider.SpawnType brakesOnSpawn =
+					equipment.IsCoupledFront || equipment.IsCoupledRear
+					? BaseControlsOverrider.SpawnType.GAME_LOAD_COUPLED
+					: BaseControlsOverrider.SpawnType.GAME_LOAD_UNCOUPLED;
+
+				baseControlsOverrider.SetBrakesOnSpawn(brakesOnSpawn);
+			}
+		}
+
+		foreach(TrainCarLivery livery in startingWagons)
+		{
+			inventory.AddMoney(CommsRadioEquipmentPurchaser.CalculateCarPrice(livery.v1));
+		}
+	}
+
+	private static IEnumerator ShowIntroductoryPopup(bool isShuntingLicenseChanged)
+	{
+		yield return new WaitForSeconds(2);
+
+		MessageBox.ShowPopupOk(
+			title: "Rolling Stock Ownership",
+			message: string.Join(" ", new string[] {
+				"It looks like this is the first time Rolling Stock Ownership has been used with this save.",
+				"You'll find that all locomotives and wagons have been removed from the world,",
+				"and that they don't magically appear at stations anymore.",
+				"They must instead be purchased.",
+				"A new mode has been added to the Comms Radio for this purpose."
+			}),
+			positive: "Understood",
+			onClose: (_) => MessageBox.ShowPopupOk(
+				title: "Rolling Stock Ownership",
+				message: string.Join(" ", new string [] {
+					isShuntingLicenseChanged ? "Since you'll need the shunting license to begin any jobs, it's been given to you." : "",
+					"A starter DE2 has been delivered to the player home, and a starting bonus has been deposited in your wallet.",
+					"Use the cash to buy a few wagons. (You won't be able to make money without them.)"
+				}),
+				positive: "Let's go!"
+			)
+		);
 	}
 }
